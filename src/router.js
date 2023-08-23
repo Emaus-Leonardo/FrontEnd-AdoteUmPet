8fc88157577f34a1583d8718eb615ef4a0f0bca1
Emaus-Leonardo <126693677+Emaus-Leonardo@users.noreply.github.com>
2023-08-23 13:40:49 -0300

    fixing errors

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './Telas/Home'
 import { CadastroProduto } from './Telas/CadastroDeProduto'
 
 
