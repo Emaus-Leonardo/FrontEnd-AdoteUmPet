8c14201ead976c52a9d12453ac63abf4a87cd643
Emaus-Leonardo <126693677+Emaus-Leonardo@users.noreply.github.com>
2023-08-23 13:51:07 -0300

    fixing img error

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CadastroProduto } from './Telas/CadastroDeProduto'
 
-
 export function Router(){
     return(
         <BrowserRouter>
